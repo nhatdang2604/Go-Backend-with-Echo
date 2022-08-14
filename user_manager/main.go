8bccb7023ff5c8da41fef4f10b3a9859b707751b
dangkl123 <nhatdang2604@gmail.com>
2022-08-14 11:47:39 +0700

    Init User Manager PJ

---
+++ user_manager/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+const (
+
+	//Address
+	PORT = "8088"
+
+	//Paths
+	ROOT_PATH = "/"
+)
+
+type Message struct {
+	Text string
+}
+
+//Handler for the root path
+func hello(ctx echo.Context) error {
+	data := Message{
+		Text: "Hello World",
+	}
+	return ctx.JSON(http.StatusOK, data)
+}
+
+func main() {
+	server := echo.New()
+	server.GET(ROOT_PATH, hello)
+	server.Logger.Fatal(server.Start(":" + PORT))
+}
