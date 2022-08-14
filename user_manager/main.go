4edf4f9bc4e2c5af08a62023d1c74754caf38023
dangkl123 <nhatdang2604@gmail.com>
2022-08-14 12:02:57 +0700

    add handler for login process

--- user_manager/main.go
+++ user_manager/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -12,11 +13,21 @@ const (
 	PORT = "8088"
 
 	//Paths
-	ROOT_PATH = "/"
+	ROOT_PATH  = "/"
+	LOGIN_PATH = "/login"
 )
 
 type Message struct {
 	Text string
+}
+
+type LoginRequest struct {
+	Username string `json:"username" form:"username" xml:"username" query: "username"`
+	Password string `json:"password" form:"password" xml:"password" query: "password"`
+}
+
+type LoginResponse struct {
+	Token string `json:"token"`
 }
 
 //Handler for the root path
@@ -27,8 +38,26 @@ func hello(ctx echo.Context) error {
 	return ctx.JSON(http.StatusOK, data)
 }
 
+func login(ctx echo.Context) error {
+
+	request := new(LoginRequest)
+	ctx.Bind(request)
+
+	log.Printf("Request data: %v\r\n", request)
+
+	if "admin" != request.Username ||
+		"admin" != request.Password {
+		return ctx.JSON(http.StatusUnauthorized, "login failed")
+	}
+
+	return ctx.JSON(http.StatusOK, &LoginResponse{
+		Token: "123456",
+	})
+}
+
 func main() {
 	server := echo.New()
 	server.GET(ROOT_PATH, hello)
+	server.POST(LOGIN_PATH, login)
 	server.Logger.Fatal(server.Start(":" + PORT))
 }
