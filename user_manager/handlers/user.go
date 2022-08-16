9ba49bacf01a74494d0b386f0357e37b0cf35f27
dangkl123 <nhatdang2604@gmail.com>
2022-08-16 13:27:23 +0700

    Add User struct to simulate fake data

--- user_manager/handlers/user.go
+++ user_manager/handlers/user.go
@@ -6,6 +6,11 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type User struct {
+	Name string
+	Age  int32
+}
+
 func GetUser(ctx echo.Context) error {
 	return ctx.String(http.StatusOK, "API Get User")
 }
