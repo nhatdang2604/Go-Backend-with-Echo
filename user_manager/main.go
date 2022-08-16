f662b22c0e4a7a328c2cce6622147241b1778b29
dangkl123 <nhatdang2604@gmail.com>
2022-08-16 21:19:04 +0700

    Change the method for Add API of user, from PUT to POST

--- user_manager/main.go
+++ user_manager/main.go
@@ -64,7 +64,7 @@ func main() {
 	//Keep grouping for User APIs
 	groupUser := server.Group(constant.USER_GROUP_PATH, isLoggedIn)
 	groupUser.GET(constant.USER_GET_PATH, handler.GetUser)
-	groupUser.PUT(constant.USER_ADD_PATH, handler.AddUser)
+	groupUser.POST(constant.USER_ADD_PATH, handler.AddUser)
 	groupUser.PUT(constant.USER_UPDATE_PATH, handler.UpdateUser, isAdmin)
 	groupUser.GET(constant.USER_DELETE_PATH, handler.DeleteUser, isAdmin)
 	groupUser.GET(constant.USER_GET_ALL_PATH, handler.GetAllUser)
